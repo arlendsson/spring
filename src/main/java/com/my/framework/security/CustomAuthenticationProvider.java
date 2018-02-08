377316f324f995953a96e27efc220f3908691877
20060059NK01\user <arlendsson@gmail.com>
2018-02-08 16:53:32 +0900

    로그인 로직 수정

--- src/main/java/com/my/framework/security/CustomAuthenticationProvider.java
+++ src/main/java/com/my/framework/security/CustomAuthenticationProvider.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.my.framework.mypage.MyPageService;
 import com.my.framework.mypage.MyPageVo;
@@ -38,25 +39,32 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		
 		MyPageVo loginUser = null;
 		try {
-			loginUser = service.selectUser(new MyPageVo(user_id, user_pw));
+			loginUser = service.selectUser(new MyPageVo(user_id, new BCryptPasswordEncoder().encode(user_pw)));
 			log.debug("########## loginUser : {}", loginUser.toString());
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new BadCredentialsException("Bad credentials - Search Login User");
 		}
 		
-//		if (StringUtils.equals("test", user_id) && StringUtils.equals("1234", user_pw)) {
 		if (loginUser != null && StringUtils.isNotEmpty(loginUser.getId())) {
-			log.debug("########## login success!");
-			List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
-			roles.add(new SimpleGrantedAuthority("ROLE_USER"));
-			
-			UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(user_id, user_pw, roles);
-			result.setDetails(new CustomUserDetails(loginUser.getId(), loginUser.getName()));
-			
-			return result;
-			
+			/**
+			 * BCryptPasswordEncoder.matches(CharSequence rawPassword, String encodedPassword) 
+			 * BCrypt.checkpw(String plaintext, String hashed)
+			 */
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+			if (encoder.matches(user_pw, loginUser.getPw())) {
+				log.debug("########## login success!");
+				List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+				roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+				
+				UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(user_id, user_pw, roles);
+				result.setDetails(new CustomUserDetails(loginUser.getId(), loginUser.getName()));
+				
+				return result;
+			} else {
+				throw new BadCredentialsException("Bad credentials - password not matched!");
+			}
 		} else {
 			log.error("########## login failed...");
 			throw new BadCredentialsException("Bad credentials");
